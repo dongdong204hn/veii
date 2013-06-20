f1ae7bbebe87e1df9bf1cc161f424acc5b1ec8ab
zhaosong <zhaosong_204@163.com>
2013-06-20 19:09:20 +0800

    .

---
+++ VOAImage/PathIntro/CommentView.h
@@ -0,0 +1,13 @@
+//
+//  CommentView.h
+//  VOAImage
+//
+//  Created by zhao song on 13-6-20.
+//  Copyright (c) 2013年 Dmitry Kondratyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentView : UIView
+
+@end
---
+++ VOAImage/PathIntro/CommentView.m
@@ -0,0 +1,31 @@
+//
+//  CommentView.m
+//  VOAImage
+//
+//  Created by zhao song on 13-6-20.
+//  Copyright (c) 2013年 Dmitry Kondratyev. All rights reserved.
+//
+
+#import "CommentView.h"
+
+@implementation CommentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
